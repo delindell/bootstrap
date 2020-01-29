f50f148ed0ce64d5717d50d8d08ae17cb0108ed0
delindell <daviselindell@gmail.com>
2020-01-28 20:17:18 -0600

    grid bootstrap

--- main.js
+++ main.js
@@ -98,3 +98,32 @@ const ducks = [
     }
 ];
 console.log(ducks);
+
+const printToDom = (divId, textToPrint) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML = textToPrint;
+
+};
+
+const duckPrinter = (quacks) => {
+    let domString = '';
+    for (let i = 0; i < quacks.length; i++) {
+        domString += '<div class=" col-md-6 col-lg-4">'
+        domString += '<div class="card">';
+        domString +=  `<img src="${quacks[i].imageUrl}" class="card-img-top" alt="...">`;
+        domString +=  '<div class="card-body">';
+        domString +=    `<h5 class="card-title">${quacks[i].name}</h5>`;
+        domString +=    `<p class="card-text">${quacks[i].socialStatus}</p>`;
+        domString +=    `<p class="card-text">${quacks[i].diet}</p>`;
+        domString +=  '</div>';
+        domString += '</div>';
+        domString += '</div>';
+    }
+    printToDom('duckCards', domString);
+}
+
+const init = () => {
+    duckPrinter(ducks);
+};
+
+init();
